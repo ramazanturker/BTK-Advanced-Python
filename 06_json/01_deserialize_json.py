a7caec5dbe1b73c2db789cd95fe508513b072fe8
ramazanturker <ramazantturker@hotmail.com>
2025-03-04 16:22:34 +0300

    json

--- 06_json/01_deserialize_json.py
+++ 06_json/01_deserialize_json.py
@@ -1,9 +1,5 @@
-""" with open("06_json/product.json") as file:
+with open("06_json/product.json") as file:
     data = file.read()
 
 print(data)
- """
-with open("/Users/turker/Desktop/BTK-Advanced-Python/06_json/product.json") as file:
-    data = file.read()
-
-print(data)
+print(type(data))
